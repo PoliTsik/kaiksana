cc5eeb02b2a2601328a8e305687b795fa21190e9
PoliTsik <tapame1988@hotmail.com>
2024-02-13 13:17:52 +0100

    second commit

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,13 @@
 from django.db import models
-from django.conf import settings
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.utils import timezone
 
 
 class Actor(models.Model):
     name = models.CharField(max_length=100)
+    GENDER_CHOICES = (('M', 'Άνδρας'), ('F', 'Γυναίκα'),)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, null=True)
+    awarded = models.BooleanField(default=False)
     def __str__(self):
         return self.name
 
@@ -26,15 +29,15 @@ class Director(models.Model):
 
 class Movie(models.Model):
     title = models.CharField(max_length=100)
-    duration = models.IntegerField()
+    duration = models.IntegerField(validators=[MinValueValidator(10),MaxValueValidator(300),])
     movie_type = models.ForeignKey(MovieType, on_delete=models.CASCADE)
     release_date = models.DateField(null=True, blank=True)
     imdb_url = models.URLField()
     image = models.ImageField(upload_to='media/', null=True)
     director = models.ForeignKey(Director, on_delete=models.CASCADE)
     stars = models.ManyToManyField(Actor)
-    imdb_rating = models.FloatField()
-    my_rating = models.FloatField()
+    imdb_rating = models.DecimalField(max_digits=3, decimal_places=1, validators=[MinValueValidator(0),MaxValueValidator(10.0)])
+    my_rating = models.DecimalField(max_digits=3, decimal_places=1, validators=[MinValueValidator(0),MaxValueValidator(10)])
     summary = models.TextField()
     user_rating = models.FloatField(null=True, blank=True)
     def __str__(self):
